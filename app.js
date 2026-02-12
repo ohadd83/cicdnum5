2834a94cb48442636524104bc296f885a6d1390c
ohad83 <ohad306@gmail.com>
2026-02-12 09:44:48 +0000

    first init

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
 http.createServer((req, res) => {
-  res.end("Hello Jenkins CI/CD lets move on ");
+  res.end("Hello Jenkins CI/CD lets move on cicd nmuber5");
 }).listen(3000);
 
